cdc8ec1337189c96ebb902cd934efbb87e933ab8
needood <tamamaspace@gmail.com>
2019-09-02 13:35:38 +0800

    bindOption 添加argv参数

--- packages/cliver/src/options.js
+++ packages/cliver/src/options.js
@@ -1,12 +1,12 @@
 import resolveFrom from 'resolve-from'
 import yargs from 'yargs'
-export function bindOptions(cli, options) {
+export function bindOptions(cli, options,argv=process.argv.slice(2)) {
     const _cli = yargs()
     options.forEach(option => {
         genOption(cli, option)
         genOption(_cli, option)
     })
-    _cli.parse(process.argv.slice(2), function (err, argv, output) {
+    _cli.parse(argv, function (err, argv, output) {
         options.forEach(option => {
             optionHandle(argv,option)
         })
